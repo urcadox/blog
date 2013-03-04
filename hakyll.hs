c4c5cc5e5f19f5201891f818ff504d35ddd01f39
Alexandre Berthaud <alexandre@berthaud.me>
2013-03-04 14:59:37 +0100

    Change: Home page title

--- hakyll.hs
+++ hakyll.hs
@@ -110,6 +110,7 @@ allPostsCtx =
 homeCtx :: Tags -> String -> Context String
 homeCtx tags list =
     constField "posts" list `mappend`
+    constField "title" "Home" `mappend`
     field "taglist" (\_ -> renderTagList tags) `mappend`
     defaultContext
 
